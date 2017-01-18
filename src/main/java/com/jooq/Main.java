2cba39877fd368f087195a710bd075421490159f
cairf <cairongfu.crf@alibaba-inc.com>
2017-01-18 16:21:57 +0800

    test jooq

--- src/main/java/com/jooq/Main.java
+++ src/main/java/com/jooq/Main.java
@@ -3,12 +3,14 @@ package com.jooq;
 import java.io.InputStream;
 
 import org.jooq.util.GenerationTool;
+import org.jooq.util.jaxb.Configuration;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         InputStream is = Main.class.getResourceAsStream("/codegen.xml");
-        GenerationTool.load(is);
+        Configuration configuration = GenerationTool.load(is);
+        GenerationTool.generate(configuration);
     }
 
 }

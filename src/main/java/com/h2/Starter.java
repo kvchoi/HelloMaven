2f89b17fcc8a2700cfd115e262357821ad738399
cairf <cairongfu.crf@alibaba-inc.com>
2017-01-19 13:49:14 +0800

    modify

--- src/main/java/com/h2/Starter.java
+++ src/main/java/com/h2/Starter.java
@@ -1,18 +1,38 @@
 package com.h2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.SQLException;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.h2.tools.Server;
+
 
 public class Starter {
 
     public static void run(String... args) {
         try {
+            System.out.println("Starting h2 server...");
+            System.out.println("Command Line args : " + ArrayUtils.toString(args));
             Server server = Server.createTcpServer(args).start();
-            new BufferedReader(new InputStreamReader(System.in)).readLine();
+            System.out.println("Started h2 server!");
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        wait4exit();
+    }
+
+    private static void wait4exit() {
+        try {
+            String tips = "press q or quit or exit then [Enter] to terminated";
+            System.out.println(tips);
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String txt = null;
+            do {
+                txt = br.readLine();
+            } while (!("q".equalsIgnoreCase(txt) || "quit".equalsIgnoreCase(txt)
+                    || "exit".equalsIgnoreCase(txt)));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }

51f9e9894be058938b0e4783e1ae0cd6422feaeb
cairf <cairongfu.crf@alibaba-inc.com>
2017-01-19 17:52:25 +0800

    modify

--- src/main/java/com/mapdb/Example.java
+++ src/main/java/com/mapdb/Example.java
@@ -12,11 +12,12 @@ public class Example {
     public static void main(String[] args) {
         mem();
         file();
+        // create form sink, usually to import some data
         sink();
     }
 
     private static void mem() {
-        DB db = DBMaker.memoryDB().make();
+        DB db = DBMaker.memoryDB().closeOnJvmShutdown().make();
         ConcurrentMap map = db.hashMap("mmap").createOrOpen();
         map.put("something", "here");
         System.out.println(map.get("something"));
@@ -24,7 +25,7 @@ public class Example {
     }
 
     private static void file() {
-        DB db = DBMaker.fileDB("file1.db").make();
+        DB db = DBMaker.fileDB("file1.db").closeOnJvmShutdown().make();
         ConcurrentMap map = db.hashMap("fmap").createOrOpen();
         map.put("something", "here");
         System.out.println(map.get("something"));
@@ -32,7 +33,7 @@ public class Example {
     }
 
     private static void sink() {
-        DB db = DBMaker.fileDB("file2.db").make();
+        DB db = DBMaker.fileDB("file2.db").closeOnJvmShutdown().make();
         // create sink
         DB.TreeMapSink<Integer, String> sink = db
                 .treeMap("smap", Serializer.INTEGER, Serializer.STRING).createFromSink();

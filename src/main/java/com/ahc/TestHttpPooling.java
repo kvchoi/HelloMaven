0f1d2d7e8876a7e6074ad58fa3c762fbdda4a036
cairf <cairongfu.crf@alibaba-inc.com>
2017-07-21 15:10:07 +0800

    测试ahc的http pooling

---
+++ src/main/java/com/ahc/TestHttpPooling.java
@@ -0,0 +1,28 @@
+package com.ahc;
+
+import java.util.concurrent.ExecutionException;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+
+/**
+ * @author by cairongfu.crf
+ * @since on 2017/7/21 14:55.
+ */
+public class TestHttpPooling {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        AsyncHttpClientConfig.Builder builder = new AsyncHttpClientConfig.Builder();
+
+        builder.setAcceptAnyCertificate(true);
+        //builder.setAllowPoolingConnections(false);
+        //builder.setAllowPoolingSslConnections(false);
+        //builder.setPooledConnectionIdleTimeout(3000);
+
+        AsyncHttpClient client = new AsyncHttpClient(builder.build());
+
+        client.prepareGet("https://www.google.com.hk/").execute().get();
+        client.prepareGet("https://www.google.com.hk/").execute().get();
+
+    }
+}

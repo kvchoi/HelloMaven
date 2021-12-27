06f33b04516b38a5b240525a7ccd9e025565be57
cairongfu <kvchoi@qq.com>
2021-12-27 13:27:09 +0800

    cache key optimal

--- src/main/java/com/caffeine/Example.java
+++ src/main/java/com/caffeine/Example.java
@@ -7,6 +7,9 @@ import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.hash.Funnels;
 import com.google.common.hash.Hashing;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.Triple;
 
 public class Example {
 
@@ -14,11 +17,22 @@ public class Example {
         LoadingCache<Object, Object> cache = Caffeine.newBuilder().maximumSize(10_000)
                 .expireAfterWrite(5, TimeUnit.MINUTES).refreshAfterWrite(3, TimeUnit.SECONDS)
                 .build(Example::createExpensiveGraph);
+        // direct set
         cache.put("key", "value");
         Object value = cache.get("key");
+
+        // loading dynamic by string key
         System.out.println(value);
         Object value2 = cache.get("key2");
         System.out.println(value2);
+
+        // loading dynamic by special key (optimal multiple partial keys than string appending)
+        Pair<String, String> pair = Pair.of("key", "key2");
+        Object value3 = cache.get(pair);
+        System.out.println(value3);
+        Triple<String, String, String> triple = Triple.of("key", "key2", "key3");
+        Object value4 = cache.get(triple);
+        System.out.println(value4);
     }
 
     private static Object createExpensiveGraph(Object key) {
